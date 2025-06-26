c1b9151cdcc02efa70c150af452f3decf8b80d8d
smashv23 <princerwanda2004@gmail.com>
2025-06-26 12:18:21 +0300

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363398106360290@newsletter',
+            newsletterName: '*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* 💥',
             serverMessageId: -1
         }
     }
