adc9f0efe1789474a47a9ae9000e45987e8d7415
smashv23 <princerwanda2004@gmail.com>
2025-06-26 11:33:31 +0300

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363398106360290@newsletter',
+            newsletterName: '*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* 💥',
             serverMessageId: -1
         }
     }
