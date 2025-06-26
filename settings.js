154f483312bed8bdbffb849bd1638305e143852d
smashv23 <princerwanda2004@gmail.com>
2025-06-26 10:31:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'SMASH',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* 💥",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255778018545', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
