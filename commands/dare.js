11d7f6af63a15a75fcebccac0d223a1064e04af4
smashv23 <princerwanda2004@gmail.com>
2025-06-26 11:34:24 +0300

    Update dare.js

--- commands/dare.js
+++ commands/dare.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function dareCommand(sock, chatId, message) {
     try {
-        const shizokeys = 'knightbot';
+        const shizokeys = '*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* 💥';
         const res = await fetch(`https://api.shizo.top/api/quote/dare?apikey=${shizokeys}`);
         
         if (!res.ok) {
