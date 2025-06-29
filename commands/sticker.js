5062b0e335d8c927cac65e04dcdf5744ac313c35
smashv23 <princerwanda2004@gmail.com>
2025-06-29 22:39:09 +0300

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -36,8 +36,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363398106360290@newsletter',
+                    newsletterName: '*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* ',
                     serverMessageId: -1
                 }
             }
